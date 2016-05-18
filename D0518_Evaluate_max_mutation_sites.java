0bfef0bb46d3e7b71d38065bd5112936ac9a0231
breezedu <breeze.du@gmail.com>
2016-05-18 10:12:42 -0400

    get max mutation sites evaluation

---
+++ D0518_Evaluate_max_mutation_sites.java
@@ -0,0 +1,57 @@
+package coursera_java_duke;
+
+
+/***********
+ * Off all the mutation sites (829 on TTN gene), some are not 'rare' enough to be considered;
+ * So we set the allele-count threshold at 10, then we got 741 qualify allele-counts
+ * if we set the allele-count threshold at 5, we got 691 qualify allele-counts
+ * if we set the allele-count threshold at 3, we got 629 qualify allele-counts
+ * 
+ * Thus, we assume allele-counts less than 5 to be 'rare'.
+ * 
+ * 
+ * 
+ * @author Jeff
+ * 05-18-2016
+ */
+public class D0518_Evaluate_max_mutation_sites {
+	
+	
+	public static void main(String[] args){
+	
+		//Initial total and threshold for our formula
+		int total = 691;
+		int threshold = 5;
+		
+		
+		//get all the combination results for Comb(n, x);
+		for(int i=0; i<5; i++){
+			long comb_rest = Combination(10, i);
+			System.out.println("Comb_10_" + i + " = " + comb_rest);
+		}
+		
+	}//end of main()
+
+	
+	/********
+	 * Get the combination of C(n, x)
+	 * @param i
+	 * @param i2
+	 * @return
+	 */
+	private static long Combination(int n, int x) {
+		// TODO Auto-generated method stub
+
+		long result = 1;
+		
+		for(int i=1; i<x + 1; i++){
+			
+			result *= ((n + 1 - i) / i);
+		}
+		
+		return result;
+	}
+
+	
+	
+}//ee
