b5aa2d5102b9c8786ebb1cc80e2d810709edfff5
breezedu <breeze.du@gmail.com>
2016-06-17 03:32:50 -0400

    update Chi-test of TTN gene R code

--- RSimulation/chi_test_of_TTN_gene.R
+++ RSimulation/chi_test_of_TTN_gene.R
@@ -3,7 +3,7 @@
 ##
 
 ## read all genes information from ALS dataset 
-ALS_data <- read.table("D:/PhD/LizDeidentified_151002/LizDeidentified_151002/gene_samp_matrix_high_LOF_hom.txt", header = TRUE, sep = "\t")
+ALS_data <- read.table("D:/PhD/LizDeidentified_151002/LizDeidentified_151002/gene_samp_matrix_high_LOF_het.txt", header = TRUE, sep = "\t")
 
 ## get TTN gene information
 ttn <- ALS_data$TTN
