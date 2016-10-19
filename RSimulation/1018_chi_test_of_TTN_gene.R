be12ec2a2e9fb9fd0c1388f0d9d3126f0716d64f
breezedu <breeze.du@gmail.com>
2016-10-19 12:07:16 -0400

    update 1018 chi-test of ttn gene R code

--- RSimulation/1018_chi_test_of_TTN_gene.R
+++ RSimulation/1018_chi_test_of_TTN_gene.R
@@ -25,7 +25,7 @@ summary(ttn_het)
 
 ##################################################################
 ## read in Pai_2|g data from txt document:
-TTN_pai2g_observed <- read.table("D:/GitHubRepositories//LossHomozygosity/ALS_dataset/TTN_Pai2g_observed.txt", header = TRUE, sep = "\t")
+TTN_pai2g_observed <- read.table("D:/GitHub/LossHomozygosity/ALS_dataset/TTN_Pai2g_observed.txt", header = TRUE, sep = "\t")
 
 ## extract only $TTN row
 TTN_pai2g_observed <- TTN_pai2g_observed$TTN
@@ -69,63 +69,68 @@ Si_mean * sqrt(length(Si))/sqrt(var(Si))
 
 ## calculate the cumulative probability P(X^2 <= CV) = 0.24
 ## http://stattrek.com/online-calculator/chi-square.aspx
-Cumulative = 0.08
+## Cumulative = 0.08
 
 
 
 sum(Si)/sqrt(length(Si)*var(Si))
-## [1] 5.263353
+## [1] 5.095161
 
 
-1-pnorm(5.263353)
-##[1] 7.072581e-08
+1-pnorm(5.095161)
+##[1] 1.742225e-07
 
 help(pnorm)
 mean(Si)
+##[1] 0.004889248
 
-
-##[1] 0.005050642
 
 mean(Si)*sqrt(length(Si))/sqrt(var(Si))
-##[1] 5.263353
+##[1] 5.095161
 
 (mean(Si)*sqrt(length(Si))/sqrt(var(Si)))^2
-##[1] 27.70288
+##[1] 25.96067
 
 (mean(Si)*sqrt(length(Si))/sqrt(var(Si)))^2/length(Si)
-##[1] 0.009091855
+##[1] 0.008520075
 
 Si_mean * sqrt(length(Si))/sqrt(var(Si))
 
 
 Si_mean = mean(Si)
 Si_mean * sqrt(length(Si))/sqrt(var(Si))
-##[1] 5.263353
+##[1] 5.095161
 
-1- pchisq(27.7, 1)
-##[1] 1.416627e-07
+1- pchisq(25.96067, 1)
+##[1] 3.484442e-07
 
 
-( 1 - pchisq(27.702, 1)) / 2
-##[1] 7.075816e-08
+( 1 - pchisq(25.96067, 1)) / 2
+##[1] 1.742221e-08
 
 ##############################################
 ## after applied fisher information 
 ## after applied Monte Carol integration
-ttn_pai2g_times_rho <- 3.6328594930727866E-5
+ttn_pai2g_times_rho <- 1.977227008827069E-4
 ttn_pai2g_sqr_montecarol <- 1.8455E-5
 
 Si_square2 <-  (TTN_pai2g_observed - ttn_pai2g_expect)^2 
 Si_square2
 mean(Si_square2)
 
-var <- ttn_pai2g_sqr_montecarol - ttn_pai2g_expect^2
-var
+var_fisher <- ttn_pai2g_sqr_montecarol - ttn_pai2g_expect^2
+var_fisher
 
-(mean(Si_square2) * sqrt(length(Si_square2))) /(ttn_pai2g_sqr_montecarol - ttn_pai2g_expect^2) 
-#8466
+## is this equation correct??
+(mean(Si_square2) * sqrt(length(Si_square2))) / sqrt(var_fisher)
 
-chi_sqr2 <- Si_square2/(ttn_pai2g_sqr_montecarol - ttn_pai2g_times_rho^2) 
+## 
+(mean(Si_square2) * sqrt(length(Si_square2))) /sqrt(ttn_pai2g_sqr_montecarol - ttn_pai2g_expect^2) 
+#36.385
+
+chi_sqr2 <- sum(Si_square2)/(ttn_pai2g_sqr_montecarol - ttn_pai2g_times_rho^2) 
 chi_sqr2
 
-1 - pchisq(8466, 1)
+1 - pchisq(36.385, 1)
+## 1.619448e-09
+############################
