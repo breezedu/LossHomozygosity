10454b12b3d260d3274a6b9be94065afdded6207
breezedu <breeze.du@gmail.com>
2017-03-27 10:35:17 -0400

    update 0327, parallel Viability sim R script

--- RSimulation/ViabilitySimulation/0327ParaViabilitySimulation_updatedPai2gRho.R
+++ RSimulation/ViabilitySimulation/0327ParaViabilitySimulation_updatedPai2gRho.R
@@ -1,13 +1,17 @@
+
 ################################
 ##
 ## 03-25-2017
 ## Viability Simulate
 ################################
-## 
+
+
+###########################################################################################
 ## Step one
 ## Read in a vector of allele frequencies.
 ## The af list was pulled down from ExAC, filtered by CCDS.r14, rare threhold set at 10%
-## 
+##
+############################################################################################
 
 TTN_af <- read.table("/work/AndrewGroup/ViabilitySimulation/QualifyTTN_variants_OnExons.txt", header = T, sep = ",")
 
@@ -20,15 +24,15 @@ variants.count <- length( TTN_af )
 
 # head(TTN_af)
 
-###############################
+###########################################################################################
 ## 
 ## Step two
 ## Pass the allele frequencies we got in step-one to a function simulateGenotypes()
 ## Return n1 (the counts of heterozygous variants), n2 (the counts of homozygous variants)
 ##        Pai2g (the sum of Pai2g * Rho) from Equation #8 in the proposal
-## 
+########################################################################################### 
 
-simulateGenocypes<-function(af.list){
+simulateGenotypes<-function(af.list){
   
   len <- length(af.list)   # the length of allele frequencies vector
   af.sim1 <- runif(n=len, min=0, max=1)               #generate a vector of random double numbers [0,1]
@@ -49,24 +53,24 @@ simulateGenocypes<-function(af.list){
     } 
     
     ### get counts for the heterozygous
+    ## check copy1
     if(af.list[i] > af.sim2[i]){
       n1 <- n1 + 1
       
       aflist.sim[i] <- aflist.sim[i] + 1
     } 
     
+    ## check copy2
     if( af.list[i] > af.sim1[i]){
       n1 <- n1 + 1
       
-      aflist.sim <- aflist.sim[i] + 1
+      aflist.sim[i] <- aflist.sim[i] + 1
     }
     
     
   } # end for i in 1:len loop; 
   
-  ## update aflist.sim from counts to frequencies by /(len * 2)
-  aflist.sim <- aflist.sim 
-  
+  ### calculate Pai2g for current person/genotype
   Pai2g <- 0
   
   if(n2 > 0){
@@ -85,8 +89,11 @@ simulateGenocypes<-function(af.list){
       
   }
   
-  if(n1 > 1)
+  ## check one case when n1>1
+  if(n1 > 1){
     print( c(n1, n2, Pai2g))
+    #print( aflist.sim )
+  }  
   #print( length( c(aflist.sim, Pai2g)))
   
   ## return a list of variants count on each know allele site, plus the calculated Pai2g for this 'person'
@@ -94,15 +101,19 @@ simulateGenocypes<-function(af.list){
   
 }
 
+## test the simulateGenotypes() function
+# for(i in 1:100000){
+#   simulateGenotypes(TTN_af)
+# }
 
-##################################################
+##################################################################
 ##
 ## step 2.5
 ## Calculate the Sum( Pai2g * Rho) for the simulated genotypes
-## 
+################################################################## 
 CalPai2gRho <- function( af.list ){
   
-  print( summary(af.list) )
+  ## print( summary(af.list) )
   
   Pai2gRho <- 1
   for(af in af.list){
@@ -113,17 +124,22 @@ CalPai2gRho <- function( af.list ){
   Pai2gRho <- ( 1 - Pai2gRho )^2
   
   return(Pai2gRho) 
-} 
+
+} ## end of CalPai2gRho() function
+
+## Pass the population allele frequencies list to the function
+## CalPai2gRho(TTN_af), got 3.632859e-05, the same result Java returned
 
 
-#####################################
+
+###########################################################################################
 ##
 ## step three
 ## 
 ## Simulate genotypes for 100k times, get n1, n2, Pai2g Matrix
 ## Perform Rao's Score Test on this set of data simulated
 ## Print the Score and P-value
-## 
+###########################################################################################
 
 simu100kGenotypes <- function(TTN_af, sample.size, variants.count){
   
@@ -133,40 +149,28 @@ simu100kGenotypes <- function(TTN_af, sample.size, variants.count){
   ## initialize pai2g.sim
   pai2g.sim <- NULL
   
-#  sim.list <- simulateGenocypes(TTN_af)
-#  print(c('sim.length', length(sim.list)) )
-  
-#  sim.matrix <- matrix( sim.list, nrow = 1, ncol = length(sim.list), byrow = T)
-  
+  ## simulate sample.size (100000) genotypes, return the Pai2gs and a vectore of allele counts for each site
   for(i in 1:sample.size){
     
-    genotype <- simulateGenocypes(TTN_af)
+    genotype <- simulateGenotypes(TTN_af)
     
     pai2g.sim <- c(pai2g.sim, genotype[1])
-    variants.sim <- variants.sim + genotype[-1]
+    genotype <- genotype[-1]
+    
+    variants.sim <- variants.sim + genotype
     
   }
-#    sim.matrix <- rbind(sim.matrix, simulateGenocypes(TTN_af))
-    
-#    sim.list <- c(sim.list, simulateGenocypes(TTN_af) )
-  
-#  sim.matrix <- matrix( sim.list, nrow = sample.size, ncol = variants.count+1, byrow = TRUE)
 
-  ## the Pai2gRho for LoF variants pai2g_expected, the very last column from the matrix
-#  col.last <- ncol(sim.matrix)
-#  TTN_pai2g.sim <- sim.matrix[ ,col.last]
-#  sim.matrix <- sim.matrix[ ,-col.last]
   
   variants.sim <- variants.sim / (sample.size * 2)
-  ttn_pai2g_exp <- 3.6328594930727866E-5  
   
+  ## ttn_pai2g_exp <- 3.6328594930727866E-5  
+  ## Pass the simulated variants alleles list to CalPai2gRho() to calculate the Sum{ Pai2g*Rho }
   ttn_pai2g_exp <- CalPai2gRho( variants.sim )
  
   TTN_pai2g.sim <- pai2g.sim 
   Si.sim <- TTN_pai2g.sim - ttn_pai2g_exp
   
-  n.sim <- length(Si.sim)
-
   ## Score.sim = ( sum(Si.sim))^2 / ( n.sim * var(Si.sim) )
   I_beta <- TTN_pai2g.sim^2 - ttn_pai2g_exp^2
 
@@ -180,37 +184,25 @@ simu100kGenotypes <- function(TTN_af, sample.size, variants.count){
 }
 
 
- 
 
-###################################################
+
+ 
+###########################################################################################
 ##
 ## step four
 ## Perform the simulation for 1000 times, see the PValues returned
-## 
+###########################################################################################
 
 PValues <- NULL
 sample.size <- 20000
 
-## Try parellel 
-library(foreach)
-library(doMC)
-registerDoMC(16)
-
-## when doing parallel, have to use list<-foreach() to catch all the returned values from all loops!
-list <- foreach( i = 1:100) %dopar% {
-
-  print(c('simulating: ', i))
-  PValues <- c(PValues, simu100kGenotypes(TTN_af, sample.size, variants.count))
-  
-}
-
-PValues <- c(PValues, unlist(list) )
 
 
-#############
+###########################################################################################
+##
 ## non parallel
 for(i in 1:20){
-  print(i)
+  print(c('simulateiong #', i) )
   PValues <- c(PValues, simu100kGenotypes(TTN_af, sample.size, variants.count))
   
 }
@@ -218,9 +210,37 @@ for(i in 1:20){
 print(PValues)
 
 mean(PValues < 0.05)
+###########################################################################################
+
+
+
+
+###########################################################################################
+## when doing parallel, have to use list<-foreach() to catch all the returned values from all loops!
+
+
+## Try parellel 
+library(foreach)
+library(doMC)
+registerDoMC(16)
+
+
+list <- foreach( i = 1:100) %dopar% {
+  
+  print(c('simulating: ', i))
+  PValues <- c(PValues, simu100kGenotypes(TTN_af, sample.size, variants.count))
+  
+}
+
+PValues <- c(PValues, unlist(list) )
+
+print(PValues)
+
+mean(PValues < 0.05)
+
 
 pdf(file = "histPvalues0326.pdf")
- hist(PValues)
+ hist(PValues, breaks = 40, main = 'Hist of P-values', xlab = paste(c('samples:', length(PValues))) )
 dev.off()
 
 mean(PValues < 0.05) 
