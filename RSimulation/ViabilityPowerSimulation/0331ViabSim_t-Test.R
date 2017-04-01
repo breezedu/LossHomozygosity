22322c7c4bb00a439c521ff1ffc7c110c854e446
breezedu <breeze.du@gmail.com>
2017-03-31 20:30:59 -0400

    add sudo pdf outputs

--- RSimulation/ViabilityPowerSimulation/0331ViabSim_t-Test.R
+++ RSimulation/ViabilityPowerSimulation/0331ViabSim_t-Test.R
@@ -17,7 +17,7 @@
 TTN_af <- read.table("/work/AndrewGroup/ViabilitySimulation/QualifyTTN_variants_OnExons.txt", header = T, sep = ",")
 
 ## loptop file
- TTN_af <- read.table("D:/PhD/QualifyTTN_variants_OnExons.txt", header = T, sep = ",")
+## TTN_af <- read.table("D:/PhD/QualifyTTN_variants_OnExons.txt", header = T, sep = ",")
 
 TTN_af <- TTN_af$Allele.Frequency
 
@@ -33,7 +33,7 @@ variants.count <- length( TTN_af )
 ##        Pai2g (the sum of Pai2g * Rho) from Equation #8 in the proposal
 ########################################################################################### 
 
-simulateGenotypes<-function(af.list){
+simulateGenotypes<-function(af.list, alpha, beta){
   
   len <- length(af.list)   # the length of allele frequencies vector
   af.sim1 <- runif(n=len, min=0, max=1)               #generate a vector of random double numbers [0,1]
@@ -100,8 +100,8 @@ simulateGenotypes<-function(af.list){
   
   #################################################
   ## Simulate the viability under the pwer, beta!=0
-  alpha <- -1.92
-  beta <- 1.80
+  ## alpha <- -1.92
+  ## beta <- 1.80
   
   ## check the Indicator
   # I <- 0
@@ -119,12 +119,12 @@ simulateGenotypes<-function(af.list){
   if( random.via < viability){
     
     return( c(Pai2g, aflist.sim))
-  
+    
   } else {
-      
-      if(Pai2g > 0)
-        print(paste(random.via, '>', viability))
-      return( simulateGenotypes(af.list) )
+    
+    if(Pai2g > 0)
+      print(paste(random.via, '>', viability))
+    return( simulateGenotypes(af.list, alpha, beta) )
     
   }
   
@@ -173,7 +173,7 @@ CalPai2gRho <- function( af.list ){
 ## Print the Score and P-value
 ###########################################################################################
 
-simu100kGenotypes <- function(TTN_af, sample.size, variants.count){
+simu100kGenotypes <- function(TTN_af, sample.size, variants.count, alpha, beta){
   
   ## initialize a vector of variants af starts at 0
   variants.sim <- rep(0, variants.count)
@@ -184,7 +184,7 @@ simu100kGenotypes <- function(TTN_af, sample.size, variants.count){
   ## simulate sample.size (100000) genotypes, return the Pai2gs and a vectore of allele counts for each site
   for(i in 1:sample.size){
     
-    genotype <- simulateGenotypes(TTN_af)
+    genotype <- simulateGenotypes(TTN_af, alpha, beta)
     
     pai2g.sim <- c(pai2g.sim, genotype[1])
     genotype <- genotype[-1]
@@ -205,11 +205,11 @@ simu100kGenotypes <- function(TTN_af, sample.size, variants.count){
   
   n.sim <- length(Si.sim)
   
-  ## score
+  ## Rao's Score Test
   #Score.sim = ( sum(Si.sim))^2 / ( n.sim * var(Si.sim) )
-   I_beta <- TTN_pai2g.sim^2 - ttn_pai2g_exp^2
+  I_beta <- TTN_pai2g.sim^2 - ttn_pai2g_exp^2
   
-   Score.sim <- ( sum(Si.sim))^2 / (sum(I_beta) )
+  Score.sim <- ( sum(Si.sim))^2 / (sum(I_beta) )
   
   ## Calculate p-values 
   p.value <- (1 - pchisq(Score.sim, df=1)) 
@@ -229,10 +229,7 @@ simu100kGenotypes <- function(TTN_af, sample.size, variants.count){
 ###########################################################################################
 
 PValues <- NULL
-sample.size <- 2000
-
-
-
+sample.size <- 200000
 ###########################################################################################
 ##
 ## non parallel
@@ -252,11 +249,11 @@ sample.size <- 2000
 #  }
 #  
 #}
-
- hist(PValues, breaks = 40, main = 'Hist of P-values', xlab = paste('samples:', length(PValues)) )
+#
+# hist(PValues, breaks = 40, main = 'Hist of P-values', xlab = paste('samples:', length(PValues)) )
 #
 #dev.off()
- mean(PValues < 0.05)
+# mean(PValues < 0.05)
 ###########################################################################################
 
 
@@ -269,19 +266,24 @@ sample.size <- 2000
 ## Try parellel 
 library(foreach)
 library(doMC)
-registerDoMC(32)
 
+## set seed
 set.seed(2017)
 
 ## Create Alpha and Beta
 alpha <- -1.922
 
-beta.vector <- (11:19) / 10
+beta.vector <- (0:21) / 10
 beta.vector
 
 
+sample.size <- 200000
+
 ######################
 for(beta in beta.vector){
+  
+  PValues <- NULL	  
+  registerDoMC(32)
   
   ##############
   ## the first 1000 samples
@@ -297,7 +299,12 @@ for(beta in beta.vector){
   ## plot Hist into a PDF document
   pdf(file = paste('histPvalues0330Power_sample1ksize200k_beta', beta, '.pdf') )
   
-  hist(PValues, breaks = 40, main = paste('Hist of P-values, breaks 40','beta=', beta), xlab = paste('samples:', length(PValues)) )
+  hist(	PValues, 
+        breaks = 40, 	
+        main = paste('Hist of P-values, breaks 40','alpha=', alpha,'beta=', beta), 
+        xlab = paste('samples:', length(PValues), 'Pvalues<0.05', mean(PValues<0.05) )	
+  )
+  
   hist(PValues, breaks = 20, main = 'Hist of P-values, breaks 20', xlab = paste('samples:', length(PValues)) ) 
   
   
