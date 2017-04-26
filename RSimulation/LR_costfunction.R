12a2a3e0763d6b1420c483bc578649be394b1e87
breezedu <breeze.du@gmail.com>
2017-04-25 20:41:21 -0400

    update linear regression gradiance Decent

--- RSimulation/LR_costfunction.R
+++ RSimulation/LR_costfunction.R
@@ -42,9 +42,54 @@ cost( -1780, -530.9, x, y)
 
 cost( -1780.0, 530.9, x, y)
 
+
+## initial theta0 theta1
+theta0 <- 0
+theta1 <- 1
+
+alpha <- mean(x)/100
+
+## repeat untill converage
+GradientDescent <- function( theta0, theta1, alpha, x, y){
+  
+  
+  temp0 <- theta0 - alpha * (1/length(x)) * sum(theta0  + theta1 * x -y)
+  
+  temp1 <- theta1 - alpha * (1/length(x)) * sum( ( theta0 + theta1*x - y) * x)
+  
+  print( c('recurssion', temp0, temp1) )
+  
+  if(abs(temp0 - theta0) < 0.00001 & abs(temp1 - theta1) < 0.00001 ){
+    
+    print( c(theta0, theta1) )
+    
+    return( c(theta0, theta1))
+    
+  } else {
+    
+    theta0 <- temp0
+    theta1 <- temp1
+    
+    #alpha <- alpha * 0.1
+    
+    GradientDescent( theta0, theta1, alpha, x, y)
+    
+  } ## end if-else conditions
+
+} ## end GradientDescent() function; 
+
+## Run GradientDescent() with paramenters and data
+coeff <- GradientDescent(theta0, theta1, alpha, x, y)
+
+coeff
+
+plot(x, y)
+
+lines( x, coeff[1] + coeff[2] * x, col = "red", lwd = 2)
+
+
+## try lme4 package
 library(lme4) 
-
-
 
 data.1 <- cbind(x, y)
 colnames(data.1) <- c("xlab", "ylab")
